87dd84bc3fbe4bba2000fbaee886956a793e09f3
unknown <bev030997@gmail.com>
2020-02-12 21:13:46 +0300

    task2 complete

--- script.js
+++ script.js
@@ -1,36 +1,54 @@
 'use strict';
 
-function DomElement(selector, height, width, bg, fontSize) {
-    this.selector = selector;
+
+
+function DomElement(height, width, bg, position) {
     this.height = height;
     this.width = width;
     this.bg = bg;
-    this.fontSize = fontSize;
+    this.position = position;
 }
 
 DomElement.prototype.createElement = function () {
 
-    let element;
-    if (this.selector.charAt(0) === '.') {
-        element = document.createElement('div');
-        element.className = this.selector.slice(1);
-    }
-
-    if (this.selector.charAt(0) === '#') {
-        element = document.createElement('p');
-        element.id = this.selector.slice(1);
-    }
-
-    element.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit';
+    let element = document.createElement('div');
+    element.className = 'square';
     element.style.cssText = `height: ${this.height}px; 
                             width: ${this.width}px; 
                             background-color: ${this.bg}; 
-                            font-size: ${this.fontSize}px`;
+                            position: ${this.position};
+                            left: 0;
+                            top: 0`;
     document.body.appendChild(element);
 };
 
-let pElement = new DomElement('#best', '600', '800', '#0000ff', '24');
-pElement.createElement();
+DomElement.prototype.moveEvents = function () {
 
-let divElem = new DomElement('.block', '200', '180', '#ffff00', '18');
-divElem.createElement();
+    document.addEventListener('keydown', function (event) {
+
+        let square = document.querySelector('.square');
+
+        if (event.keyCode === 37) {
+            square.style.left = (parseInt(square.style.left) - 10) + 'px';
+        } // left
+        if (event.keyCode === 38) {
+            square.style.top = (parseInt(square.style.top) - 10) + 'px';
+        } //up
+        if (event.keyCode === 39) {
+            square.style.left = (parseInt(square.style.left) + 10) + 'px';
+
+        } // right
+        if (event.keyCode === 40) {
+            square.style.top = (parseInt(square.style.top) + 10) + 'px';
+        } // down
+    });
+};
+
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    let element = new DomElement('100', '100', '#4b0082', 'absolute');
+    element.createElement();
+    element.moveEvents();
+});
